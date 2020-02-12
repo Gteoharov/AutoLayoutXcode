44e74403b79f712e10ac1c13a8efa647fa7dc82e
Gteoharov <georgia.teoharov@gmail.com>
2020-02-12 22:54:37 +0200

    layouts

--- PirateHandbook/WeatherViewController.swift
+++ PirateHandbook/WeatherViewController.swift
@@ -39,33 +39,32 @@ final class WeatherViewController: UIViewController {
     imageViews.forEach(view.addSubview)
     spacerGuides.forEach(view.addLayoutGuide)
     
-    
     NSLayoutConstraint.activate(
-        NSLayoutConstraint.constraints(
-            withVisualFormat: """
-            |[leftGap]\
-            -[monday(fullWidth@849)]-(bigGap)\
-            -[tuesday(smaller@849)]-(gap)-[wednesday(smaller@849)]\
-            -(gap)-[rightGap(==leftGap)]|
-            """
-            ,
-            options: .alignAllBottom,
-            metrics: {
-                let imageWidth = imageViews[0].image!.size.width
-                let gap: CGFloat = 8
-                return [
-                    "fullWidth": imageWidth,
-                    "smaller": imageWidth * 0.7,
-                    "bigGap": gap * 3,
-                    "gap": gap
-                ]
-            }(),
-            views: Dictionary(uniqueKeysWithValues: [
-                (["monday", "tuesday", "wednesday"], imageViews),
-                (["leftGap", "rightGap"], spacerGuides)
-                ].flatMap {(keys, values: [Any]) in zip(keys, values) }
-            )
+      NSLayoutConstraint.constraints(
+        withVisualFormat: """
+          |[leftGap]\
+          -[monday(fullWidth@849)]-(bigGap)\
+          -[tuesday(smaller@849)]-(gap)-[wednesday(smaller@849)]\
+          -(gap)-[rightGap(==leftGap)]|
+          """,
+        options: .alignAllBottom,
+        metrics: {
+          let imageWidth = imageViews[0].image!.size.width
+          let gap: CGFloat = 8
+          return [
+            "fullWidth": imageWidth,
+            "smaller": imageWidth * 0.7,
+            "bigGap": gap * 3,
+            "gap": gap
+          ]
+        }(),
+        views: Dictionary(
+          uniqueKeysWithValues: [
+            (["monday", "tuesday", "wednesday"], imageViews),
+            (["leftGap", "rightGap"], spacerGuides)
+          ].flatMap {(keys, values: [Any]) in zip(keys, values)}
         )
+      )
       + [imageViews[0].centerYAnchor.constraint(equalTo: view.centerYAnchor)]
       + imageViews.map {$0.heightAnchor.constraint(equalTo: $0.widthAnchor)}
     )
